140bf691edc9e4b0347e2f0484228446aa4bb42b
Vítor Marques <dleggdraw@gmail.com>
2025-11-04 10:47:29 -0300

    WORKFLOW

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,6 @@
-const app = require('./app').default || require('./app');
+import app from './app.js';
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-export {};
 //# sourceMappingURL=index.js.map
